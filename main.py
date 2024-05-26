0211603f60e9c2249a24ad8a4addeb15e9e0753e
HowCam <3604402972@qq.com>
2024-05-27 07:30:41 +0800

    add: 添加二模倒计时

--- main.py
+++ main.py
@@ -5,12 +5,12 @@ class CountdownApp:
     def __init__(self):
         self.root = tk.Tk()
         self.root.title("倒计时器")
-        self.root.geometry("600x80")  # 设置窗口大小
+        self.root.geometry("600x160")  # 设置窗口大小
         self.root.configure(bg='black')  # 设置窗口背景色为黑色
         self.root.overrideredirect(True)  # 去掉窗口边框和标题栏
 
-        # 设置窗口默认置顶
-        self.root.attributes("-topmost",True)
+        # # 设置窗口默认置顶
+        # self.root.attributes("-topmost",True)
 
         # 获取屏幕的宽度和高度
         screenWidth = self.root.winfo_screenwidth()
@@ -19,7 +19,7 @@ class CountdownApp:
         # 计算窗口位置，使其居中
         self.root.geometry(f"+{screenWidth//2 - 250}+0")
 
-        self.label = tk.Label(self.root, text="距离中考还有：", font=("仿宋", 24), fg='yellow', bg='black')
+        self.label = tk.Label(self.root, font=("仿宋", 24), fg='yellow', bg='black')
         self.label.pack(pady=20)
 
         # 读取时间文件
@@ -38,7 +38,7 @@ class CountdownApp:
             days = remaining_time.days
             hours, remainder = divmod(remaining_time.seconds, 3600)
             minutes, seconds = divmod(remainder, 60)
-            self.label.config(text=f"距离中考还有：{days}天 {hours}小时 {minutes}分钟 {seconds}秒")
+            self.label.config(text=f"距离中考还有：{days}天 {hours}小时 {minutes}分钟 {seconds}秒\n距离二模还有：{days-8}天 {hours}小时 {minutes}分钟 {seconds}秒")
 
         self.root.after(1000, self.update_countdown)
 
